ad4f29620eaecb6ec2dc2874d8792bb1ead2482d
marcos.asdes <marco.engc@poli.ufrj.br>
2022-08-10 19:36:18 -0300

    starting a new project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { json } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import 'express-async-errors'
+
+/* import router from './routes/index.js'
+import errorHandler from './events/appError.js' */
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(json())
+app.use(helmet())
+/* app.use(router)
+app.use(errorHandler) */
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import app from './app.js'
+
+/* import appLog from './events/appLog.js' */
+
+const PORT: Number = +process.env.PORT || 4000
+
+/* app.listen(PORT, () => {
+  appLog('Server', `Server listening on port ${PORT}`)
+}) */
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
