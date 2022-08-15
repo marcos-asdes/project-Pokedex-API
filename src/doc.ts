18d7363d0b50398b59cd6f26ccb19bb5f2aa121e
marcos.asdes <marco.engc@poli.ufrj.br>
2022-08-15 02:17:55 -0300

    doc: Added simplified application documentation on the route  https://project21r-pokedex-api.herokuapp.com/

---
+++ src/doc.ts
@@ -0,0 +1,31 @@
+export const doc = `
+-- Pokedex API --
+
+endpoints: 
+
+- method post - sign up 
+https://project21r-pokedex-api.herokuapp.com/sign-up
+expect json on format:
+{
+  "email": "email@example.com",
+  "password": "password_example",
+  "confirmPassword": "password_example"
+}
+
+- method post - sign in 
+https://project21r-pokedex-api.herokuapp.com/sign-in
+expect json on format: 
+{
+  "email": "email@example.com",
+  "password": "password_example"
+}
+
+- method get - get all pokemons in database, user token required
+https://project21r-pokedex-api.herokuapp.com/pokemons
+
+- method post - add pokemon in user's collection, user token required and pokemon id required
+https://project21r-pokedex-api.herokuapp.com/my-pokemons/:id/add
+
+- method post - remove pokemon from user's collection, user token required and pokemon id required
+https://project21r-pokedex-api.herokuapp.com/my-pokemons/:id/remove
+` 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
+import { Request, Response } from 'express'
+
 import app from './app.js'
 
 import appLog from './events/appLog.js'
+import { doc } from './doc.js'
 
 const PORT: Number = +process.env.PORT || 4000
 
 app.listen(PORT, () => {
   appLog('Server', `Server listening on port ${PORT}`)
+})
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send(`
+  Application online. 
+  Server listening on port ${PORT}.
+  
+  ${doc}
+  `)
 })
