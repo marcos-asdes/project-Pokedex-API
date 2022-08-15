36e92358d9b0b9c89f08145a085f5098885f6dd3
marcos.asdes <marco.engc@poli.ufrj.br>
2022-08-15 04:19:19 -0300

    doc: altered documentation layout and the rendering response of the '/' route

--- doc.ts
+++ doc.ts
@@ -1,4 +1,4 @@
-export const doc = `
+const doc = `
 -- Pokedex API --
 
 endpoints: 
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import { Request, Response } from 'express'
 import app from './app.js'
 
 import appLog from './events/appLog.js'
-import { doc } from './doc.js'
 
 const PORT: Number = +process.env.PORT || 4000
 
@@ -15,7 +14,5 @@ app.get('/', (_req: Request, res: Response) => {
   res.send(`
   Application online. 
   Server listening on port ${PORT}.
-  
-  ${doc}
   `)
 })
