aeda1534a98598fcbfee6defb6e6fc28f4e65843
marcos.asdes <marco.engc@poli.ufrj.br>
2023-03-09 23:33:52 -0300

    update: dependencies have been updated

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import app from './app.js'
 
 import appLog from './events/appLog.js'
 
-const PORT: Number = +process.env.PORT || 4000
+const PORT: number = +process.env.PORT || 5000
 
 app.listen(PORT, () => {
   appLog('Server', `Server listening on port ${PORT}`)
