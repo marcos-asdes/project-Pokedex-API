c461eeff53d695d382ccbdd12e35fd2e9966a7ba
marcos.asdes <marco.engc@poli.ufrj.br>
2023-03-23 14:22:03 -0300

    refactor: seed has been updated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,34 +4,40 @@ import client from '../src/config/database.js'
 import appLog from '../src/events/appLog.js'
 
 async function popPokedex() {
-  const pokelist = 20
+  const pokelist = 350
 
-  await client.$transaction([
-    client.$executeRaw`TRUNCATE pokemons CASCADE`
-  ]);
+  await client.pokemon.deleteMany()
 
   for(let i=1; i<=pokelist; i++) {
     const pokeAPI = `https://pokeapi.co/api/v2/pokemon/${i}/`
     axios.get(pokeAPI)
-    .then(async(res) =>{
-      const data = res.data
-      await client.pokemon.create({
-        data: {
-          id: data.id,
-          name: data.name,
-          number: data.order,
-          image: data.sprites.front_default,
-          weight: data.weight,
-          height: data.height,
-          baseExp: data.base_experience,
-          description: "no description available"
-        }
+      .then(async(res) => {
+        const data = res.data
+        await client.pokemon.create({
+          data: {
+            id: data.id,
+            name: data.name,
+            number: data.order,
+            image: data.sprites.front_default,
+            weight: data.weight,
+            height: data.height,
+            baseExp: data.base_experience,
+            description: 'no description available'
+          }
+        })
       })
-    })
-    .catch(error => appLog('Error', `${error}`))
+      .catch(error => appLog('Error', `${error}`))
   }
 }
 
 popPokedex()
+  .then(async () => {
+    await client.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await client.$disconnect()
+    process.exit(1)
+  })
 
 export { popPokedex }
